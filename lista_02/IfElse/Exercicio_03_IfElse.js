4e702a2a146fcf00e0fca3c35d909dee09a95136
franciellivalerio <franciellivaleriodeoliveira@gmail.com>
2024-06-27 16:48:56 -0300

    Correção do código com adição de mais uma condicional.

--- lista_02/IfElse/Exercicio_03_IfElse.js
+++ lista_02/IfElse/Exercicio_03_IfElse.js
@@ -9,23 +9,29 @@ function possibilidadeDeDoar(nomeDoador, idadeDoador, tipoDeDoacao) {
     nomeDoador = nomeDoador.toUpperCase();
     tipoDeDoacao = tipoDeDoacao.toLowerCase();
 
-        if (idadeDoador < 60 && idadeDoador <= 69 && tipoDeDoacao == "sim") {
-            
-            return "\n Você nao esta apto para doar sangue!";
-        
-        } else if (idadeDoador < 16) {
-            
-            return "\n Você não pode doar sangue, pois e menor de idade.";
-        
-        } else if (idadeDoador > 69) {
-            
-            return "\n Você não pode doar sangue, pois e maior de idade.";
-        
-        } else if (tipoDeDoacao != "sim" && tipoDeDoacao != "nao") {
-            
-            return "\n Resposta invalida. Por favor, responda 'sim' ou 'nao'.";
-        
-}
+
+    if (idadeDoador >= 60 && idadeDoador <= 69 && tipoDeDoacao == "nao") {
+
+        return "\n Você esta apto para doar sangue!";
+
+    } else if (idadeDoador >= 60 && idadeDoador <= 69 && tipoDeDoacao == "sim") {
+
+        return "\n Você nao esta apto para doar sangue!";
+
+
+    } else if (idadeDoador < 16) {
+
+        return "\n Você não pode doar sangue, pois e menor de idade.";
+
+    } else if (idadeDoador > 69) {
+
+        return "\n Você não pode doar sangue, pois e maior de idade.";
+
+    } else if (tipoDeDoacao != "sim" && tipoDeDoacao != "nao") {
+
+        return "\n Resposta invalida. Por favor, responda 'sim' ou 'nao'.";
+
+    }
 }
 
 let nomeDoador = leia.question("\n Por favor, digite seu nome: ");
